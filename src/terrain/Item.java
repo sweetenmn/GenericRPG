c0682253cbe1eb98f92055509e3f729def660960
vXenonv <devin.henderson@rocketmail.com>
2015-12-06 13:39:01 -0600

    Item enum
    Changed Item to an enum class

--- src/terrain/Item.java
+++ src/terrain/Item.java
@@ -1,15 +1,31 @@
 package terrain;
 
-import game.Drawable;
+import javafx.scene.image.Image;
 
-public class Item extends Drawable{
-	public String name;
-	public int value;
-	public String description;
-	
-	public Item(Item i){
-		this.name = i.name;
-		this.description = i.description;
-		this.value = i.value;
+public enum Item{
+	Health("Health Potion", "Heals the Hero for 10 health.", 10){
+		public Image getAvatar(){
+			return new Image("assets/health_potion");
+		};
+	},
+	Experience("Experience Potion", "Grants the Hero additional experience", 10){
+		public Image getAvatar(){
+			return new Image("assets/exp_potion");
+		};
+	};
+		private String name;
+		private String description;
+		private int value;
+		
+		public abstract Image getAvatar();
+		public String getName(){return name;}
+		public String getDescription(){return description;}
+		public int value(){return value;}
+				
+		Item(String name, String description, int value){
+			this.name = name;
+			this.description = description;
+			this.value = value;
+		}
 	}
-}
+
